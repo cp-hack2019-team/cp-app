9892b1e98f935c1ce85f96f8541f27b76b838b10
Anton <anton.zubarev@crm-uni.de>
2019-06-08 17:51:51 +0300

    Follow commit #2

---
+++ src/app/_interfaces/follower.ts
@@ -0,0 +1,4 @@
+export interface Follower {
+    id: number;
+    name: string;
+}
---
+++ src/app/_interfaces/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    uid: string | number;
+    login: string;
+    firstName: string;
+    lastName: string;
+    birthday: Date;
+    email: string;
+}
