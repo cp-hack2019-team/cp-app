bb53c0b8390488d1b5fe23794dc51401f67aad04
nikchernyakov <nikchernyakov@yandex.ru>
2019-06-08 12:15:13 +0300

    Create rest service template

---
+++ src/app/services/rest.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {Observable, timer} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+const apiUrl = 'http://hack-university-server.herokuapp.com/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(public http: HttpClient) { }
+
+  static getApiUrl() {
+    return apiUrl;
+  }
+
+  getEvent() {
+    return new Promise((resolve, reject) => {
+      // let headers = new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem('token'));
+      this.http.get(RestService.getApiUrl() + "concert-info")
+          .subscribe(res => {
+            //	console.log(res); // log
+            resolve(res);
+          }, (err) => {
+            console.log(err); // log
+            reject(err);
+          });
+    });
+  }
+
+  signIn(name) {
+    return new Promise((resolve, reject) => {
+      this.http.get(RestService.getApiUrl() + 'registration/' + name.userName)
+          .subscribe(res => {
+            console.log(res); // log
+            resolve(res);
+          }, (err) => {
+            console.log(err); // log
+            reject(err);
+          });
+    });
+  }
+
+  sendMsg(message) {
+    return new Promise((resolve, reject) => {
+      let headers = new HttpHeaders();
+      headers.append('content-type', 'application/json');
+      let body = new HttpParams();
+      body = body.set('text', message.text);
+      body = body.set('userId', message.userId);
+      this.http.post(RestService.getApiUrl() + 'send-msg', body, {headers: headers})
+          .subscribe(res => {
+            console.log(res); // log
+            resolve(res);
+          }, (err) => {
+            console.log(err); // log
+            reject(err);
+          });
+    });
+  }
+
+  getRequest(url: string) {
+    return new Promise((resolve, reject) => {
+      // let headers = new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem('token'));
+      this.http.get(url)
+          .subscribe(res => {
+            //	console.log(res); // log
+            resolve(res);
+          }, (err) => {
+            console.log(err); // log
+            reject(err);
+          });
+    });
+  }
+
+}
