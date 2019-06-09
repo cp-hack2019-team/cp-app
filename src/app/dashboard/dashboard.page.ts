9a1fe970e82d6b115a073c23998f7cc387eabaad
Alex <Ksh_sa@mail.ru>
2019-06-09 12:00:18 +0300

    User and dash pages design

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+	
+	todoData = [{title : 'Сегодня, 9 июня', description : 'На сегодня всё'},{title : 'Завтра, 10 июня', description : 'Поход к врачу', time : '15:00'}];
 
   constructor() { }
 
